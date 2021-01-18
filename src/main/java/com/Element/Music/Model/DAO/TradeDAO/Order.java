ab5a806540d78788223bd30bcd210f24a2bc9369
jiangjiayi1995 <740612415@qq.com>
2021-01-19 00:28:41 +0800

    完善新增DAO

--- src/main/java/com/Element/Music/Model/DAO/TradeDAO/Order.java
+++ src/main/java/com/Element/Music/Model/DAO/TradeDAO/Order.java
@@ -1,6 +1,36 @@
 package com.Element.Music.Model.DAO.TradeDAO;
 
 import com.Element.Music.Model.DAO.BaseEntity;
+import com.Element.Music.Model.DAO.MusicDAO.Song;
+import com.Element.Music.Model.DAO.UserDAO.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity(name = "Order")
+@Data
+@Builder
+@Table(name = "Order")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order extends BaseEntity {
+
+    @OneToOne
+    private Song song;
+
+    @OneToOne
+    private Consumer consumer;
+
+    private Long orderCode;
+
+    private Double payPrice;//实付金额，考虑一下重新取名字
+
+    private Double discount;//优惠金额，就是原价-实付金额
+
 }
--- src/main/java/com/Element/Music/Model/DAO/TradeDAO/Price.java
+++ src/main/java/com/Element/Music/Model/DAO/TradeDAO/Price.java
@@ -1,7 +1,28 @@
 package com.Element.Music.Model.DAO.TradeDAO;
 
 import com.Element.Music.Model.DAO.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity(name = "Price")
+@Table(name = "Price")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Price extends BaseEntity {
 
+    private Double originalPrice;
+
+    private Double rate;//平台收取的比例
+
+    private Double showPrice;//含义为最终展示的价格，没有做discount之前
+
+    public Double getShowPrice() {
+        this.showPrice = originalPrice * (1 + rate);
+        return showPrice;
+    }
 }
